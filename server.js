2d3a6020e2817188f93e9db71729277c8a80877c
Josh Forbord <48929764+r0110@users.noreply.github.com>
2019-04-01 16:44:25 -0500

    Modify message

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8888;
 
-app.get('/', (req, res) => res.send('Hello Mankato!'));
+app.get('/', (req, res) => res.send('This is frontEnd!'));
 
 app.listen(port);
